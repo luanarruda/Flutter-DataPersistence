5200de4dc1f73b5301e8ceb5545199f977dbe011
luanarruda <luanamarrudaa@gmail.com>
2025-02-11 11:52:47 -0300

    Explored Stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -17,32 +17,51 @@ class MyApp extends StatelessWidget {
       ),
       home: Scaffold(
         appBar: AppBar(title: Text('Tarefas')),
-        body: Container(
-          child: Stack(
-            children: [
-              Container(color: Colors.pink, height: 140),
-              Container(
-                color: Colors.white,
-                height: 100,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween, //vai alinhar tudo dessa linha, o retangulo
-                  children: [
-                    Container(
-                      color: Colors.black26,
-                      width: 72,
-                      height: 100,
-                    ),
-                    Text('Aprender Flutter'),
-                    ElevatedButton(onPressed: (){}, child: Icon(Icons.arrow_drop_up)),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Task('Aprender Flutter'),
+            Task('Andar de bike'),
+            Task('Meditar'),
 
-                  ],
-                ),
-              )
-            ],
-          ),
+          ],
         ),
         floatingActionButton: FloatingActionButton(onPressed: () {}),
       ),
     );
   }
 }
+
+class Task extends StatelessWidget {
+  final String nome;
+  const Task( this.nome, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return  Container(
+      child: Stack(
+        children: [
+          Container(color: Colors.pink, height: 140),
+          Container(
+            color: Colors.white,
+            height: 100,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween, //vai alinhar tudo dessa linha, o retangulo
+              children: [
+                Container(
+                  color: Colors.black26,
+                  width: 72,
+                  height: 100,
+                ),
+                Text(nome),
+                ElevatedButton(onPressed: (){}, child: Icon(Icons.arrow_drop_up)),
+
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
