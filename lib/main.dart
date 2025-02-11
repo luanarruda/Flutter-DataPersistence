6ee8ab76ad6812e99b4d3f3f5ab910dc988dd52d
luanarruda <luanamarrudaa@gmail.com>
2025-02-11 12:47:07 -0300

    Explored linearProgressIndicator

--- lib/main.dart
+++ lib/main.dart
@@ -85,15 +85,35 @@ class _TaskState extends State<Task> {
                             print(nivel);
                           },
                           child: Icon(Icons.arrow_drop_up)),
+
                     ],
                   ),
+
                 ),
-                Text(
-                  'Nivel: $nivel',
-                  style: TextStyle(color: Colors.white, fontSize: 16),
-                )
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8),
+                      child: Container(
+                        child: LinearProgressIndicator(
+                          color: Colors.white,
+                          value: nivel/10,
+                        ),
+                          width: 200,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(10),
+                      child: Text(
+                        'Nivel: $nivel',
+                        style: TextStyle(color: Colors.white, fontSize: 16),
+                      ),
+                    ),
+                  ],
+                ),
               ],
-            )
+            ),
           ],
         ),
       ),
