2020100cc9b399381a37d6eb0d2d8010ca3d5e02
luanarruda <luanamarrudaa@gmail.com>
2025-02-11 12:11:43 -0300

    Explored ListView and Padding

--- lib/main.dart
+++ lib/main.dart
@@ -17,13 +17,15 @@ class MyApp extends StatelessWidget {
       ),
       home: Scaffold(
         appBar: AppBar(title: Text('Tarefas')),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        body: ListView(
           children: [
-            Task('Aprender Flutter'),
+            Task(
+                'Aprender Flutter no café da manha jogando bola dando mortal para trás'),
             Task('Andar de bike'),
             Task('Meditar'),
-
+            Task('Meditar'),
+            Task('Meditar'),
+            Task('Meditar'),
           ],
         ),
         floatingActionButton: FloatingActionButton(onPressed: () {}),
@@ -34,34 +36,55 @@ class MyApp extends StatelessWidget {
 
 class Task extends StatelessWidget {
   final String nome;
-  const Task( this.nome, {Key? key}) : super(key: key);
+  const Task(this.nome, {Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return  Container(
-      child: Stack(
-        children: [
-          Container(color: Colors.pink, height: 140),
-          Container(
-            color: Colors.white,
-            height: 100,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween, //vai alinhar tudo dessa linha, o retangulo
+    int nivel
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        child: Stack(
+          children: [
+            Container(color: Colors.pink, height: 140),
+            Column(
               children: [
                 Container(
-                  color: Colors.black26,
-                  width: 72,
+                  color: Colors.white,
                   height: 100,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment
+                        .spaceBetween, //vai alinhar tudo dessa linha, o retangulo
+                    children: [
+                      Container(
+                        color: Colors.black26,
+                        width: 72,
+                        height: 100,
+                      ),
+                      Container(
+                          width: 200,
+                          child: Text(
+                            nome,
+                            style: TextStyle(
+                              fontSize: 24,
+                              overflow: TextOverflow
+                                  .ellipsis, //coloca tres pontinhos no texto q ficou muito grande
+                            ),
+                          )),
+                      ElevatedButton(
+                          onPressed: () {}, child: Icon(Icons.arrow_drop_up)),
+                    ],
+                  ),
                 ),
-                Text(nome),
-                ElevatedButton(onPressed: (){}, child: Icon(Icons.arrow_drop_up)),
-
+                Text(
+                  'Nivel: o',
+                  style: TextStyle(color: Colors.white, fontSize: 16),
+                )
               ],
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
 }
-
