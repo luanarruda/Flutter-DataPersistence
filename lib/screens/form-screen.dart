4d6a7a7210513aa7575efccef2123c795295b8d3
luanarruda <luanamarrudaa@gmail.com>
2025-02-24 14:03:01 -0300

    Explored keyboardType

--- lib/screens/form-screen.dart
+++ lib/screens/form-screen.dart
@@ -19,90 +19,95 @@ class _FormScreenState extends State<FormScreen> {
         title: const Text('Nova tarefa'),
       ),
       body: Center(
-        child: Container(
-          height: 650,
-          width: 375,
-          decoration: BoxDecoration(
-            color: Colors.black12,
-            borderRadius: BorderRadius.circular(10),
-            border: Border.all(width: 3),
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextFormField(
-                  controller: nameController,
-                  textAlign: TextAlign.center,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: 'Nome',
-                    fillColor: Colors.white70,
-                    filled: true,
+        child: SingleChildScrollView(
+          // deixa a tela scrollavel, quando abrir o teclado a tela nao se ajusta para tudo caber na tela
+          child: Container(
+            height: 650,
+            width: 375,
+            decoration: BoxDecoration(
+              color: Colors.black12,
+              borderRadius: BorderRadius.circular(10),
+              border: Border.all(width: 3),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    controller: nameController,
+                    textAlign: TextAlign.center,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Nome',
+                      fillColor: Colors.white70,
+                      filled: true,
+                    ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextFormField(
-                  controller: difficultyController,
-                  textAlign: TextAlign.center,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: 'Dificuldade',
-                    fillColor: Colors.white70,
-                    filled: true,
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    keyboardType: TextInputType.number,//o teclado vira teclado so de numeros
+                    controller: difficultyController,
+                    textAlign: TextAlign.center,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Dificuldade',
+                      fillColor: Colors.white70,
+                      filled: true,
+                    ),
                   ),
-                ),
-              ), //tem o validator
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextFormField(
-                  onChanged: (text){
-                   setState(() {
-
-                   });
-                  },//on changed, qunado mudar usa oset state ja que estamos em um statefullwidget
-                  controller: imageController,
-                  textAlign: TextAlign.center,
-                  //input
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: 'Imagem',
-                    fillColor: Colors.white70,
-                    filled: true,
-                  ),
-                ),
-              ),
-              Container(
-                height: 100,
-                width: 72,
-                decoration: BoxDecoration(
-                  color: Colors.pinkAccent,
-                  borderRadius: BorderRadius.circular(10),
-                  border: Border.all(width: 2, color: Colors.pink),
-                ),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(10),
-                  child: Image.network(
-                    imageController.text,
-                    errorBuilder: (BuildContext context, Object excepetion, StackTrace? stackTrace){
-                      return Image.asset('assets/images/noimage.svg.png');
+                ), //tem o validator
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    onChanged: (text) {
+                      setState(() {});
                     },
-                    fit: BoxFit.cover,
+                    //on changed, qunado mudar usa oset state ja que estamos em um statefullwidget
+                   keyboardType: TextInputType.url,
+                    controller: imageController,
+                    textAlign: TextAlign.center,
+                    //input
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Imagem',
+                      fillColor: Colors.white70,
+                      filled: true,
+                    ),
                   ),
                 ),
-              ),
-              ElevatedButton(
-                  onPressed: () {
-                    print(nameController);
-                    print(int.parse(difficultyController.text));
-                    print(imageController);
-                  },
-                  child: const Text('Adicionar'))
-            ],
+                Container(
+                  height: 100,
+                  width: 72,
+                  decoration: BoxDecoration(
+                    color: Colors.pinkAccent,
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(width: 2, color: Colors.pink),
+                  ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: Image.network(
+                      imageController.text,
+                      errorBuilder: (BuildContext context, Object excepetion,
+                          StackTrace? stackTrace) {
+                        return Image.asset('assets/images/noimage.svg.png');
+                      },
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      print(nameController);
+                      print(int.parse(difficultyController.text));
+                      print(imageController);
+                    },
+                    child: const Text('Adicionar'))
+              ],
+            ),
           ),
         ),
       ),
