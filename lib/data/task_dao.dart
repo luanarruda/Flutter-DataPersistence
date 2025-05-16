0578e86ea8aff82b8d389de33a2b0736a8e312a8
luanarruda <luanamarrudaa@gmail.com>
2025-05-16 13:16:32 -0300

    using the Read method

--- lib/data/task_dao.dart
+++ lib/data/task_dao.dart
@@ -1,5 +1,7 @@
 //responsavel por qq responsabilidade de comunicao com o bd
 //1° responsabilidade = tabela
+import 'package:sqflite/sqflite.dart';
+
 import '../components/task.dart';
 
 class TaskDao {
@@ -19,7 +21,21 @@ class TaskDao {
 
   save(Task tarefa) async {}
   //retorna uma lista de tarefas do bd
-  Future<List<Task>> findAll() async{}
+  Future<List<Task>> findAll() async{
+    print('Acessando o FindAll: ');
+    final Database bancoDeDados = await getDatabase(); //await pq é um feature, entao tem q esperar o banco de dados ser encontrado ou construido para vincular no nosso db
+
+    //os objetos que vamos pegar do db, vem em tipo de Mapa, ent cada tarefa tem uma imagem, nome, valor especifico
+    //a chave do mapa sempre serao strings e os valores podem ser alterados(dynamic)
+    //mapa do tpo string e dinamico
+    final List<Map<String,dynamic>> result = await bancoDeDados.query(_tablename)
+    print('Procurando dados no banco de dados... encontrado: $result');
+
+    // o metodo find all precisa retornar algo - todo metodo precisa retornar algo
+    // por ser feature, demora um tempo para retornar a lista de tarefas
+    //na vdd agr retorna mapa (map), entao precisamos transformar
+    return toList()
+  }
   //procura por uma tarefa
   Future<List<Task>> find(String nomeDaTarefa) async{}
   delete(String nomeDaTarefa) async{}
